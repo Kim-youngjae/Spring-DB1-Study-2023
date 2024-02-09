da27dd4faf640e5e81c9fdd110e21c2441fe5af3
Kim-youngjae <youngjae459@gmail.com>
2024-02-09 22:28:36 +0900

    수정, 삭제 실습 & test

--- src/main/java/hello/jdbcstudy/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbcstudy/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbcstudy.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 
 @Slf4j
@@ -24,6 +25,76 @@ public class MemberRepositoryV0 {
             pstmt.setInt(2, member.getMoney());
             pstmt.executeUpdate(); // 실행할 sql 전달
             return member;
+        } catch (SQLException e) {
+            log.error("db error");
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery(); // 결과 응답을 받을 때 사용하는 메서드
+
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error");
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate(); // 쿼리를 실행하고 영향을 받은 row 수를 반환한다.
+            log.info("resultSize={}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error");
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate(); // 쿼리를 실행하고 영향을 받은 row 수를 반환한다.
         } catch (SQLException e) {
             log.error("db error");
             throw e;
--- src/test/java/hello/jdbcstudy/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbcstudy/repository/MemberRepositoryV0Test.java
@@ -4,8 +4,10 @@ import hello.jdbcstudy.domain.Member;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MemberRepositoryV0Test {
 
@@ -15,6 +17,17 @@ class MemberRepositoryV0Test {
     void crud() throws SQLException {
         Member member = new Member("member1", 10000);
         repository.save(member);
+
+        Member findMember = repository.findById("member1");
+        assertThat(findMember).isEqualTo(member);
+
+        repository.update("member1", 20000);
+        Member updatedMember = repository.findById("member1");
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 
 }
